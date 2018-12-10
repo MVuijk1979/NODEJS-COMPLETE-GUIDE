de1691dbe3ac2ea048206262bbb18f4f092b178a
MVuijk <CG57QV@INSIM.BIZ>
2018-12-10 22:11:21 +0100

    converted html files to pug

--- 41-understanding-npm-scripts/app.js
+++ 41-understanding-npm-scripts/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 // const adminRoutes = require('./routes/admin');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
