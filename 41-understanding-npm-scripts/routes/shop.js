54fc633a0c51ba660e0ae4abae4ae0624c52875d
Marcel Vuijk <mvuijk@hotmail.com>
2018-12-13 23:49:05 +0100

    finished 88

--- 41-understanding-npm-scripts/routes/shop.js
+++ 41-understanding-npm-scripts/routes/shop.js
@@ -9,7 +9,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/shop' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
